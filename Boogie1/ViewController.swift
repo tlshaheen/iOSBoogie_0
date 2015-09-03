ccd430c28584b8dc1f481e26b1b1ee4423b73ee0
Arvind Subramanian <arvind@weddingwire.com>
2015-09-02 22:09:49 -0400

    Adding hints

--- Boogie1/ViewController.swift
+++ Boogie1/ViewController.swift
@@ -35,6 +35,29 @@ class ViewController: UIViewController {
     so 'The' and 'the' count as the same word.
     */
     func findTheDuplicates(fileContents: String) {
+
+        // There are a bunch of ways to do this, but here's one:
+        // let's break down the problem
+        
+        // 1. take the giant string and turn it into individual words
+        // 2. make a dictionary of [words : count]
+        // 3. loop through each word in the giant string
+        // 3. make each word lower case
+        // 4. remove whitespace surrounding each word
+        // 5. add an entry in the dictionary for each word and increment the 'count' each time you see it again in the dictionary
+        // 6. once you're done get an array of all the words in the dictionary (all the keys)
+        // 7. sort the words alphabetically
+        // 8. loop through the array and print out the count for each word
+        
+        // Some useful documentation:
+        // see a stackoverflow for the split method: http://stackoverflow.com/questions/25678373/swift-split-a-string-into-an-array
+        
+        
+        // removing whitespace from a string: http://stackoverflow.com/questions/28570973/how-should-i-remove-all-the-spaces-from-a-string-swift
+        
+        
+        // to sort an array in swift: http://stackoverflow.com/questions/25223058/how-to-sort-an-array-in-swift
+        
         
     }
     
